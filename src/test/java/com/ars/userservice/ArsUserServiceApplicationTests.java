172e42e538d80a62ee7b70b3a3e09c49d3876fb9
thoaidc <dcthoai1023@gmail.com>
2025-08-29 23:08:04 +0700

    init

---
+++ src/test/java/com/ars/userservice/ArsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
